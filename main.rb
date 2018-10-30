f3c61ddc084170eb19061cbdb2091de8553ab377
Katsuki <Katsuki_Sato0130@icloud.com>
2018-10-31 07:43:12 +0900

    Added gitignore

--- main.rb
+++ main.rb
@@ -1,3 +1,13 @@
 require 'countdown_timer'
+require 'twitter'
+require_relative 'keys.rb'
 
-p CountdownTimer::Timer.set_timer(2018,11,16)
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = CONSUMER_KEY
+  config.consumer_secret     = CONSUMER_SECRET
+  config.access_token        = ACCESS_TOKEN
+  config.access_token_secret = ACCESS_SECRET
+end
+
+countdown = CountdownTimer::Timer.set_timer(2018, 11, 16)
+client.update("I will see you in #{countdown}")
